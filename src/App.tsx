271cc48d0797eb966816f05b95a2835b4496fdfb
yakshao <akshyapatill@gmail.com>
2024-08-07 03:59:59 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,97 +2,150 @@
 import { ChangeEvent, useState } from 'react';
 import './App.css'
 
+
 function App() {
-  const [description, setDescription] = useState<any>();
+  const [content, setContent] = useState<any>();
   const [img, setImg] = useState<any>();
   const [og, setOg] = useState<string>();
-
+  const [loading, setLoading] = useState(false)
+  const [title, setTitle] = useState<any>();
 
   let form = new FormData();
-  let url = 'https://backog.onrender.com/genOG'
+  let url = 'http://localhost:3000/genOG'
 
   async function handleSubmit() {
+
+    if(!title || !content){
+      alert('Title or Content should not be empty..');
+      return;
+    }
+
+    setLoading(true);
+
+    form.append(
+      'title', title
+    )
 
     form.append(
       'img', img
     )
+
     form.append(
-      'desc', description
+      'content', content
     )
 
-
- const response =  await fetch(url, {
+    const response = await fetch(url, {
       method: 'POST',
       body: form
     }
     )
-    
-  const dResponse = await response.json();
 
-setOg(dResponse.url);
-window.open(dResponse.url, '_blank')?.focus(); // okay also
+    const dResponse = await response.json();
 
-
-    }
+    setOg(dResponse.url);
+    setLoading(false)
+  }
 
 
   return (
     <>
-    <div>
-      <form id='uploadForm'>
-        <h2>Upload a file</h2>
+      <div className='container'>
+        <div className='form'>
+          <form id='uploadForm'>
+            <h2>OG Generator</h2>
 
-        <input
-          type='file'
-          onChange={(e: ChangeEvent) =>
-            setImg(
-              (e.target as HTMLInputElement)!.files![0]
-            )
-          }
-        />
+            <div style={{alignSelf: 'center' }}>
 
-        <label>
-          Description:
-          <input
-            id='desc'
-            type='text'
-            name='Descrition'
-            placeholder='Description'
-            onChange={(e) => {
-              setDescription(e.currentTarget.value
-              )
-            }
+              <h5
+                style={{ margin: 10 }}
+              >Title</h5>
 
-            }
-          /></label>
-        <button
-          type='submit'
-          onClick={(e) => {
-            e.preventDefault();
-            handleSubmit();
-          }}
 
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+              <input
+                style={{ width: '400px', padding: '20px', borderRadius: '10px' }}
+                id='desc'
+                name='Descrition'
+                type='text'
+                placeholder='Title'
+                onChange={(e) => {
+                  setTitle(e.currentTarget.value
+                  )
+                }
+                }
+              />
 
-  {og && 
-  <div>
- 
+            </div>
 
-      <img 
-      width={1200}
-      height={630}
-      src= {og}
+            <div style={{  margin: '20px', alignSelf: 'center' }}>
 
-     />
-    
-    </div>}
+              <h5
+                style={{ margin: 10 }}
+              >Content</h5>
+
+
+              <textarea
+                style={{ width: '400px', height: '100px', padding: '20px', borderRadius: '10px' }}
+                id='desc'
+                name='Descrition'
+                placeholder='Content'
+                onChange={(e) => {
+                  setContent(e.currentTarget.value
+                  )
+                }
+                }
+              />
+
+            </div>
+
+            <h5
+                style={{ margin: 0 }}
+              >Image</h5>
+            <div style={{ padding: '20px', border: '1px solid', borderRadius: '10px', margin: '20px' }}>
+
+              <input
+
+                type='file'
+                onChange={(e: ChangeEvent) =>
+                  setImg(
+                    (e.target as HTMLInputElement)!.files![0]
+                  )
+                }
+              />
+            </div>
+            <button
+              type='submit'
+              onClick={(e) => {
+                e.preventDefault();
+                handleSubmit();
+              }}
+              style={{ border: '1px solid' }}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+
+        {
+          loading &&
+          <div className='center'>
+            <div>
+              <div className="loader"></div>
+            </div>
+          </div>
+        }
+
+        {og &&
+          <div
+            style={{ paddingTop: 30 }}
+          >
+            <img
+              src={og}
+            />
+
+          </div>}
+      </div>
     </>
   )
-  
 }
 
 export default App
